2942e13148ae5e663e5547dc47f5ef1d326e333f
Eagu Kim <egkim@arontier.co>
2019-12-19 10:13:57 +0900

    housekeeping

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ "cromwell" == "$(basename $1)" ]; then
   shift
-  exec gosu cromwell /usr/bin/java ${JAVA_OPTS} -jar /app/cromwell.jar ${CROMWELL_ARGS} "$@"
+  exec gosu cromwell /bin/bash -c 'java ${JAVA_OPTS} -jar /app/cromwell.jar ${CROMWELL_ARGS} "$@" | tee -a /var/log/cromwell/cromwell.log'
 fi
 
 exec "$@"
