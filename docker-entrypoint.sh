0a9a5821a3edec841722ca7a779a86c21c309858
Eagu Kim <egkim@arontier.co>
2019-12-19 11:32:25 +0900

    housekeeping

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
+#trap "exit 1" TERM INT PIPE EXIT
+
 if [ "cromwell" == "$(basename $1)" ]; then
-  exec gosu cromwell /bin/bash -c 'java ${JAVA_OPTS} -jar /app/cromwell.jar ${CROMWELL_ARGS} "$@" | tee -a /var/log/cromwell/cromwell.log' "$@"
+  exec gosu cromwell /bin/bash -c 'java ${JAVA_OPTS} -jar /app/cromwell.jar ${CROMWELL_ARGS} "$@" 2<&1 | tee -a /var/log/cromwell/cromwell.log' "$@"
 fi
 
 exec "$@"
